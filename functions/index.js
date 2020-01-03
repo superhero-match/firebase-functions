df0368dce1ff3cb31747cd906bb6c2e8aa905d55
MWSOFT <mwsoftofficial@gmail.com>
2020-01-03 18:04:36 +0100

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,129 @@
+const functions = require('firebase-functions');
+
+let admin = require("firebase-admin");
+
+admin.initializeApp(functions.config().firebase);
+
+
+exports.newMatch = functions.https.onRequest((req, resp) => {
+    console.log(req.body);
+
+    let request = JSON.parse(req.body);
+
+    let token = request.token;
+    console.log("token:", token);
+
+    var options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24
+    };
+
+    const payload = {
+        data: {
+            data_type: "new_match",
+            matched_user_id: request.matchedUserId
+        }
+    };
+
+    return admin.messaging().sendToDevice(token, payload, options)
+    .then(function(response) {
+        console.log("Successfully sent message:", response);
+        resp.statusCode(200).send();
+      })
+    .catch(function(error) {
+        console.log("Error sending message:", error);
+        resp.statusCode(500).send();
+      });
+});
+
+exports.deleteMatch = functions.https.onRequest((req, resp) => {
+    console.log(req.body);
+
+    let request = JSON.parse(req.body);
+
+    let token = request.token;
+    console.log("token:", token);
+    
+    var options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24
+    };
+
+    const payload = {
+        data: {
+            data_type: "delete_match",
+            matched_user_id: request.matchedUserId
+        }
+    };
+
+    return admin.messaging().sendToDevice(token, payload, options)
+    .then(function(response) {
+        console.log("Successfully sent message:", response);
+        resp.statusCode(200).send();
+      })
+    .catch(function(error) {
+        console.log("Error sending message:", error);
+        resp.statusCode(500).send();
+      });
+});
+
+exports.newLike = functions.https.onRequest((req, resp) => {
+    console.log(req.body);
+
+    let request = JSON.parse(req.body);
+
+    let token = request.token;
+    console.log("token:", token);
+
+    var options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24
+    };
+
+    const payload = {
+        data: {
+            data_type: "new_like",
+            matched_user_id: request.matchedUserId
+        }
+    };
+
+    return admin.messaging().sendToDevice(token, payload, options)
+    .then(function(response) {
+        console.log("Successfully sent message:", response);
+        resp.statusCode(200).send();
+      })
+    .catch(function(error) {
+        console.log("Error sending message:", error);
+        resp.statusCode(500).send();
+      });
+});
+
+exports.newMessage = functions.https.onRequest((req, resp) => {
+    console.log(req.body);
+
+    let request = JSON.parse(req.body);
+
+    let token = request.token;
+    console.log("token:", token);
+
+    var options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24
+    };
+
+    const payload = {
+        data: {
+            data_type: "new_message"
+        }
+    };
+
+    return admin.messaging().sendToDevice(token, payload, options)
+    .then(function(response) {
+        console.log("Successfully sent message:", response);
+        resp.statusCode(200).send();
+      })
+    .catch(function(error) {
+        console.log("Error sending message:", error);
+        resp.statusCode(500).send();
+      });
+});
